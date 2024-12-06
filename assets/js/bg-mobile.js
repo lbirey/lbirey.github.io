071cacd693d86e470121fd350944d85849d903af
lbirey <140372008+lbirey@users.noreply.github.com>
2024-12-06 17:43:28 +0800

    Update bg-mobile.js

--- assets/js/bg-mobile.js
+++ assets/js/bg-mobile.js
@@ -1,7 +1,17 @@
-// 设置固定的背景图片URL
-const backgroundImageUrl = 'https://api.suyanw.cn/api/sjbz.php';
+function getRandomBackgroundImage() {
+    const images = [
+        '../img/bei.jpg',
+        '../img/bei1.jpg',
+        '../img/bei2.jpg',
+       '../img/bei3.jpg',
+       '../img/bei4.jpg',
+    ];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+}
 
 // 设置背景图
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.style.setProperty('--bg-image', `url(${backgroundImageUrl})`);
+    const randomImage = getRandomBackgroundImage();
+    document.body.style.setProperty('--bg-image', `url('${randomImage}')`);
 });
